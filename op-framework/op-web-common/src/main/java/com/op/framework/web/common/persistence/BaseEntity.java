25919a5401d40fb1642d697fda9bbbbd716ae387
cdrcool <cdr_cool@163.com>
2021-05-19 01:35:04 +0800

    增加 mybatis generator lombock 插件

--- op-framework/op-web-common/src/main/java/com/op/framework/web/common/persistence/BaseEntity.java
+++ op-framework/op-web-common/src/main/java/com/op/framework/web/common/persistence/BaseEntity.java
@@ -45,5 +45,5 @@ public class BaseEntity implements Serializable {
     /**
      * 租户id
      */
-    private Long tenantId;
+    private String tenantId;
 }
---
+++ op-framework/op-web-common/src/main/java/com/op/framework/web/common/persistence/mybatis/plugins/LombokPlugin.java
@@ -0,0 +1,81 @@
+package com.op.framework.web.common.persistence.mybatis.plugins;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.Interface;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.springframework.util.StringUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author cdrcool
+ */
+public class LombokPlugin extends PluginAdapter {
+
+    @Override
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    @SuppressWarnings("AlibabaRemoveCommentedCode")
+    @Override
+    public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+//        topLevelClass.addImportedType("java.io.Serializable");
+        topLevelClass.addImportedType("lombok.Builder");
+        topLevelClass.addImportedType("lombok.Data");
+        topLevelClass.addImportedType("lombok.EqualsAndHashCode");
+        topLevelClass.addImportedType("lombok.NoArgsConstructor");
+        topLevelClass.addAnnotation("@Builder");
+        topLevelClass.addAnnotation("@NoArgsConstructor");
+        topLevelClass.addAnnotation("@EqualsAndHashCode(callSuper = true)");
+        topLevelClass.addAnnotation("@Data");
+        topLevelClass.addJavaDocLine("/**");
+        topLevelClass.addJavaDocLine(" * @author Mybatis Generator");
+        topLevelClass.addJavaDocLine(" * @date " + (new SimpleDateFormat("yyyy/MM/dd hh:mm")).format(new Date()));
+        topLevelClass.addJavaDocLine(" */");
+//        topLevelClass.addSuperInterface(new FullyQualifiedJavaType("java.io.Serializable"));
+
+        return true;
+    }
+
+    @Override
+    public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass,
+                                       IntrospectedColumn introspectedColumn,
+                                       IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        field.getJavaDocLines().clear();
+
+        String remark = introspectedColumn.getRemarks();
+        if (StringUtils.hasText(remark)) {
+            field.addJavaDocLine("/**");
+            field.addJavaDocLine(" * " + remark);
+            field.addJavaDocLine(" */");
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean clientGenerated(Interface interfaze, IntrospectedTable introspectedTable) {
+        interfaze.addJavaDocLine("/**");
+        interfaze.addJavaDocLine(" * @author Mybatis Generator");
+        interfaze.addJavaDocLine(" * @date " + (new SimpleDateFormat("yyyy/MM/dd hh:mm")).format(new Date()));
+        interfaze.addJavaDocLine(" */");
+        return true;
+    }
+
+    @Override
+    public boolean modelGetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        return false;
+    }
+
+    @Override
+    public boolean modelSetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        return false;
+    }
+}
