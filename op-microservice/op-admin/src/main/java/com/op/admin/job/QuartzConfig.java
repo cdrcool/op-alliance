c53d9ee3a8d4684cf93d637822c5a0e2ddc60fc9
chengdr01 <chendgr01@mingyuanyun.com>
2021-08-20 11:56:10 +0800

    支持 quartz 分布式

--- op-microservice/op-admin/src/main/java/com/op/admin/job/QuartzConfig.java
+++ op-microservice/op-admin/src/main/java/com/op/admin/job/QuartzConfig.java
@@ -1,27 +1,65 @@
-//package com.op.admin.job;
-//
-//import org.quartz.*;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-///**
-// * Quartz 任务调度配置类
-// *
-// * @author chengdr01
-// */
-//@Configuration
-//public class QuartzConfig {
-//    @Bean
-//    public JobDetail xxJobDetail() {
-//        return JobBuilder.newJob(XxJob.class).withIdentity("xxJob").storeDurably().build();
-//    }
-//
-//    @Bean
-//    public Trigger testQuartzTrigger(JobDetail xxJobDetail) {
-//        CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule("*/10 * * * * ?");
-//        return TriggerBuilder.newTrigger().forJob(xxJobDetail)
-//                .withIdentity("xxJob")
-//                .withSchedule(cronScheduleBuilder)
-//                .build();
-//    }
-//}
+package com.op.admin.job;
+
+import org.quartz.spi.JobFactory;
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+
+import javax.sql.DataSource;
+
+/**
+ * @author chengdr01
+ */
+@Configuration
+public class QuartzConfig {
+
+    @Bean
+    public JobFactory jobFactory(ApplicationContext applicationContext) {
+        AutoWiredSpringBeanToJobFactory jobFactory = new AutoWiredSpringBeanToJobFactory();
+        jobFactory.setApplicationContext(applicationContext);
+        return jobFactory;
+    }
+
+    @Bean(destroyMethod = "destroy", autowire = Autowire.NO)
+    public SchedulerFactoryBean schedulerFactoryBean(JobFactory jobFactory, DataSource dataSource) {
+        SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
+        schedulerFactoryBean.setJobFactory(jobFactory);
+        schedulerFactoryBean.setAutoStartup(true);
+        schedulerFactoryBean.setStartupDelay(1);
+        schedulerFactoryBean.setOverwriteExistingJobs(true);
+        schedulerFactoryBean.setDataSource(dataSource);
+        schedulerFactoryBean.setApplicationContextSchedulerContextKey("applicationContext");
+        schedulerFactoryBean.setConfigLocation(new ClassPathResource("/quartz.properties"));
+        return schedulerFactoryBean;
+    }
+
+    /**
+     * 继承 org.springframework.scheduling.quartz.SpringBeanJobFactory
+     * 实现任务实例化方式
+     */
+    public static class AutoWiredSpringBeanToJobFactory extends SpringBeanJobFactory implements ApplicationContextAware {
+        private transient AutowireCapableBeanFactory beanFactory;
+
+        @Override
+        public void setApplicationContext(final ApplicationContext context) {
+            beanFactory = context.getAutowireCapableBeanFactory();
+        }
+
+        @Override
+        protected Object createJobInstance(final TriggerFiredBundle bundle) throws Exception {
+            final Object job = super.createJobInstance(bundle);
+            // 将 job 实例交付给 Spring 容器
+            beanFactory.autowireBean(job);
+            return job;
+        }
+
+    }
+
+}
---
+++ op-microservice/op-admin/src/main/java/com/op/admin/job/QuartzConfigOld.java
@@ -0,0 +1,27 @@
+//package com.op.admin.job;
+//
+//import org.quartz.*;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * Quartz 任务调度配置类
+// *
+// * @author chengdr01
+// */
+//@Configuration
+//public class QuartzConfigOld {
+//    @Bean
+//    public JobDetail xxJobDetail() {
+//        return JobBuilder.newJob(XxJob.class).withIdentity("xxJob").storeDurably().build();
+//    }
+//
+//    @Bean
+//    public Trigger testQuartzTrigger(JobDetail xxJobDetail) {
+//        CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule("*/10 * * * * ?");
+//        return TriggerBuilder.newTrigger().forJob(xxJobDetail)
+//                .withIdentity("xxJob")
+//                .withSchedule(cronScheduleBuilder)
+//                .build();
+//    }
+//}
