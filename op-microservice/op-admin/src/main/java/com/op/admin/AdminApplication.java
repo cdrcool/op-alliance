de70fcfe6cde792b394415cd5f98d6e55352600f
chengdr01 <chendgr01@mingyuanyun.com>
2021-10-08 14:30:01 +0800

    调整 async 配置

--- op-microservice/op-admin/src/main/java/com/op/admin/AdminApplication.java
+++ op-microservice/op-admin/src/main/java/com/op/admin/AdminApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @author cdrcool
  */
 @EnableScheduling
+@EnableAsync
 @ServletComponentScan
 @EnableCaching
 @EnableDiscoveryClient
